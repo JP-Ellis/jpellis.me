57c0e7bfdef8cf277f31d2e27a1f4d639701b3e0
JP-Ellis <josh@jpellis.me>
2023-03-18 18:35:10 +1100

    feat: add MyAppBar
    Signed-off-by: JP-Ellis <josh@jpellis.me>

---
+++ src/lib/components/MyAppBar/MyAppBar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { AppBar, LightSwitch } from "@skeletonlabs/skeleton";
+</script>
+
+<AppBar>
+  <!-- Lead Section -->
+  <svelte:fragment slot="lead">
+    <span class="text-xl">
+      Joshua
+      <strong class="text-xl uppercase">Ellis</strong>
+    </span>
+  </svelte:fragment>
+
+  <!-- Trail Section -->
+  <svelte:fragment slot="trail">
+    <LightSwitch />
+    <a
+      class="btn variant-ghost-surface"
+      href="https://github.com/JP-Ellis"
+      target="_blank"
+    >
+      GitHub
+    </a>
+    <a
+      class="btn variant-ghost-surface"
+      href="mailto:website@jpellis.me"
+      target="_blank"
+    >
+      Email
+    </a>
+    <a
+      class="btn variant-ghost-surface"
+      href="https://au.linkedin.com/in/joshuapellis"
+      target="_blank"
+    >
+      LinkedIn
+    </a>
+  </svelte:fragment>
+</AppBar>
