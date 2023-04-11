f4aeddd6120fc72de8b38d22351cc3717204d9ee
JP-Ellis <josh@jpellis.me>
2023-04-12 08:45:36 +1000

    chore(deps): add svg support
    Using @poppanator/sveltekit-svg
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -3,3 +3,29 @@ declare module "$lib/assets/images/*" {
   const meta: object[];
   export default meta;
 }
+
+declare module "$lib/assets/*.svg?component" {
+  import type { ComponentType, SvelteComponentTyped } from "svelte";
+  import type { SVGAttributes } from "svelte/elements";
+
+  const content: ComponentType<
+    SvelteComponentTyped<SVGAttributes<SVGSVGElement>>
+  >;
+
+  export default content;
+}
+
+declare module "*.svg?src" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.svg?url" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.svg?dataurl" {
+  const content: string;
+  export default content;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import svg from "@poppanator/sveltekit-svg";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { imagetools } from "@zerodevx/svelte-img/vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [sveltekit(), imagetools()],
+  plugins: [sveltekit(), imagetools(), svg()],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
   },
