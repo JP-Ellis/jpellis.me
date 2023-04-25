726f63d17b2f1d8d2013f0447485bf0ea3eb59d3
JP-Ellis <josh@jpellis.me>
2023-04-26 08:25:01 +1000

    feat: add error page
    Signed-off-by: JP-Ellis <josh@jpellis.me>

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+{#if $page.error}
+  <div
+    class="flex flex-col justify-center align-center mx-auto h-[calc(100vh-74px)]"
+  >
+    <div class="text-center text-xl pb-6">There was an error</div>
+    <div class="flex flex-row content-center">
+      <h1 class="w-1/2 inline-block text-end border-r pr-4 border-primary-500">
+        {$page.status}
+      </h1>
+      <h2 class="w-1/2 inline-block pl-4">{$page.error.message}</h2>
+    </div>
+  </div>
+{:else}
+  <div
+    class="flex flex-col justify-center align-center mx-auto h-[calc(100vh-74px)]"
+  >
+    <div class="text-center text-xl border-b border-primary-500 pb-6 mx-auto">
+      There was an error
+    </div>
+    <h1 class="text-center mx-auto pt-6">
+      {$page.status}
+    </h1>
+  </div>
+{/if}
