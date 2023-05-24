b4fccaa6169e38c56efe293ee6150e58832a90a8
JP-Ellis <josh@jpellis.me>
2023-05-24 20:36:17 +1000

    chore: remove adapter-static
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
 import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
-// const dev = process.argv.includes("dev");
-// if (dev) {
-//   process.env.BASE_PATH = "";
-// }
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors for more
+  // information about preprocessors
   preprocess: [vitePreprocess()],
 
   kit: {
@@ -20,17 +15,6 @@ const config = {
         exclude: ["<all>"],
       },
     }),
-    // Static
-    // adapter: adapter({
-    //   pages: "build",
-    //   assets: "build",
-    //   fallback: null,
-    //   precompress: true,
-    //   strict: true,
-    // }),
-    // paths: {
-    //   base: process.env.BASE_PATH,
-    // },
   },
 };
 
