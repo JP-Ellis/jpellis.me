5f0bc5a049b48bddef44e2b021e97cdfe71108f9
JP-Ellis <josh@jpellis.me>
2023-06-18 19:41:32 +1000

    feat: add tikz-feynman project
    Signed-off-by: JP-Ellis <josh@jpellis.me>

---
+++ src/routes/projects/tikz-feynman/tikz-feynman.pdf/+page.server.ts
@@ -0,0 +1,11 @@
+import { redirect } from "@sveltejs/kit";
+
+import type { PageServerLoad } from "./$types";
+
+export const load = (({ url }) => {
+  const version =
+    url.searchParams.get("v") ?? url.searchParams.get("version") ?? "latest";
+
+  // eslint-disable-next-line @typescript-eslint/no-throw-literal, no-magic-numbers
+  throw redirect(301, `/manual/tikz-feynman/${version}.pdf`);
+}) satisfies PageServerLoad;
