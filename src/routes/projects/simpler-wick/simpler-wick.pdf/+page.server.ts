966bc6a4af9350e203f0fae6d8c28c5562f6dbf5
JP-Ellis <josh@jpellis.me>
2023-06-19 07:58:05 +1000

    fix: pdf names
    When downloading the manual, the file would just be called `latest.pdf`
    which is not very helpful.
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/routes/projects/simpler-wick/simpler-wick.pdf/+page.server.ts
+++ src/routes/projects/simpler-wick/simpler-wick.pdf/+page.server.ts
@@ -4,8 +4,8 @@ import type { PageServerLoad } from "./$types";
 
 export const load = (({ url }) => {
   const version =
-    url.searchParams.get("v") ?? url.searchParams.get("version") ?? "latest";
+    url.searchParams.get("v") ?? url.searchParams.get("version") ?? "1.0.0";
 
   // eslint-disable-next-line @typescript-eslint/no-throw-literal, no-magic-numbers
-  throw redirect(301, `/manual/simpler-wick/${version}.pdf`);
+  throw redirect(301, `/manual/simpler-wick-${version}.pdf`);
 }) satisfies PageServerLoad;
--- src/routes/projects/tikz-feynman/tikz-feynman.pdf/+page.server.ts
+++ src/routes/projects/tikz-feynman/tikz-feynman.pdf/+page.server.ts
@@ -4,7 +4,7 @@ import type { PageServerLoad } from "./$types";
 
 export const load = (({ url }) => {
   const version =
-    url.searchParams.get("v") ?? url.searchParams.get("version") ?? "latest";
+    url.searchParams.get("v") ?? url.searchParams.get("version") ?? "1.1.0";
 
   // eslint-disable-next-line @typescript-eslint/no-throw-literal, no-magic-numbers
   throw redirect(301, `/manual/tikz-feynman/${version}.pdf`);
