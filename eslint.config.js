89a1330d5c53c9fc94c30b137a2c197d163e09c8
Karl Horky <karl.horky@gmail.com>
2023-11-11 17:14:20 +0100

    Fix typo

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import sveltePlugin from "eslint-plugin-svelte";
 import svelteParser from "svelte-eslint-parser";
 
 export default [
-  // Glocal ignores
+  // Global ignores
   {
     ignores: [".svelte-kit/**/*"],
   },
